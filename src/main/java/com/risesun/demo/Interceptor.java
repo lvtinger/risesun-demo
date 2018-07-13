0960413c2763044bf468a716fcb4ca3bf3fc368c
邱旭 <a.qiu1981@icloud.com>
2018-07-13 20:39:54 +0800

    。。。

--- src/main/java/com/risesun/demo/Interceptor.java
+++ src/main/java/com/risesun/demo/Interceptor.java
@@ -1,5 +1,9 @@
 package com.risesun.demo;
 
+import java.util.Properties;
+
 public interface Interceptor {
     Object intercept(Invocation invocation) throws Throwable;
+    Object plugin(Object target);
+    void setProperties(Properties properties);
 }
---
+++ src/main/java/com/risesun/demo/Intercepts.java
@@ -0,0 +1,4 @@
+package com.risesun.demo;
+
+public @interface Intercepts {
+}
